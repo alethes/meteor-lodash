038765aa057cd7edaefa78c0f30e8d6304f9c4e1
alethes <freelancer.alethes@gmail.com>
2013-11-13 04:20:26 +0100

    New version

--- package.js
+++ package.js
@@ -2,10 +2,7 @@ Package.describe({
   summary: "A utility library delivering consistency, customization, performance, & extras. Can be a drop-in place replacement for Underscore.js."
 });
 
-Npm.depends({'lodash': '2.3.0'});
-
 Package.on_use(function (api) {
-  api.use('meteor', {unordered: true});
   api.export('_');
   api.add_files(["lib/lodash.js"], ["client", "server"]);
 });
